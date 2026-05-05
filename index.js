b60c85ae08516231247b3f75c83ff4c3423f891b
alexandraperez-commits <alexandra.perez@utec.edu.pe>
2026-05-04 21:03:34 -0500

    index.js

--- index.js
+++ index.js
@@ -4,15 +4,32 @@ const app = express();
 function obtenerMensaje() {
   return "¡Hola Mundo desde Node.js en la nube!";
 }
+
 function obtenerHora() {
-  return "Hora actual: 20:51";
+  const ahora = new Date();
+  const hora = ahora.getHours().toString().padStart(2, '0');
+  const minutos = ahora.getMinutes().toString().padStart(2, '0');
+  return `Hora actual: ${hora}:${minutos}`;
 }
-function Date() {
-  return "Día: 4/05/2026"
+
+function obtenerFecha() {
+  const ahora = new Date();
+  const dia = ahora.getDate().toString().padStart(2, '0');
+  const mes = (ahora.getMonth() + 1).toString().padStart(2, '0'); // Los meses van de 0 a 11
+  const anio = ahora.getFullYear();
+  return `Día: ${dia}/${mes}/${anio}`;
 }
 
 app.get("/", (req, res) => {
   res.send(obtenerMensaje());
+});
+
+app.get("/hora", (req, res) => {
+  res.send(obtenerHora());
+});
+
+app.get("/fecha", (req, res) => {
+  res.send(obtenerFecha());
 });
 
 app.get("/health", (req, res) => res.status(200).send("ok"));
